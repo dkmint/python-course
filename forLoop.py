42e5551e15d5c06776b1a6212d809bc163a98e03
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2020-12-02 20:08:21 +0100

    Add files via upload
    For Loop

---
+++ forLoop.py
@@ -0,0 +1,17 @@
+#for item in 'Python':
+#    print(item)
+#for string in ['Mosh', 'Sarah', 'John']:
+#    print(string)
+#for n in [1, 2, 3, 4, 5]:
+#    print(n)
+#for i in range(10):
+#    print(i)
+#for k in range(5, 10):
+#    print(k)
+#for l in range(5, 10, 2):
+#    print(l)
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price
+print(f'Total: {total}')
