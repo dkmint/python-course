f28ec81f9207990869a3a0048a391b6006064dee
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2020-12-09 19:14:35 +0100

    Add files via upload
    Count Plot Example

---
+++ countPlot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings('ignore')
+%matplotlib inline
+
+df = pd.read_csv('auto_clean.csv')
+df.head()
+
+sns.countplot(y = 'cylinders', data=df)
+plt.show()
