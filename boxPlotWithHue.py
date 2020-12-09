78806f092fad9f8dc273f4e0954aeaac40533218
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2020-12-09 19:17:04 +0100

    Add files via upload
    Box Plot with hue Parameter

---
+++ boxPlotWithHue.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings('ignore')
+%matplotlib inline
+
+df = pd.read_csv('auto_clean.csv')
+df.head()
+
+sns.boxplot(x='origin', y='mpg', data=df, hue='cylinders')
+plt.show()
