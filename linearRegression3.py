ad5f60fa91387f9cafce13610c890276a060fb22
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2020-12-10 22:15:07 +0100

    Add files via upload
    Linear Regression 3

---
+++ linearRegression3.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import sklearn.datasets as data
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+
+wine = data.load_wine()
+type(wine)
+wine.keys()
+print(wine.DESCR)
+wine.data
+wine.feature_names
+df = pd.DataFrame(wine.data, columns=wine.feature_names)
+df['wine_type'] = wine.target
+wine.target
+df.info()
+df.head()
+sns.pairplot(df)
+# plt.show()
+
+reg = LinearRegression()
+reg
+X = df['proline']
+y = df['alcohol']
+X # here X is Pandas's series; so it's like 1D; it's wrong, we need to convert data frame.
+reg.fit(X,y)
+# Cause of error: sklearn requirements is X should be a 2D array, or Pandas DataFrame, or a Matrix; 
+#it should be 2D tabel of data
+
+#y can be 1D array or 1D DataFrame
+X = df[['proline']]
+y = df['alcohol']
+X # here X is Pandas's series; so it's like 1D; it's wrong, we need to convert data frame.
+reg.fit(X, y)
+pred = reg.predict(X) # save this into pred variable
+
+from sklearn import metrics
+X = df.drop(['alcohol', 'wine_type'], axis=1) # Let's save it to X
+reg.fit(X, y)
+pred = reg.predict(X)
+metrics.r2_score(y, pred)
