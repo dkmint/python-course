70c8947b960f8e977735a17afd5213b9d1426919
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2020-12-11 20:00:07 +0100

    Add files via upload
    Local and Global Variables Illustration

---
+++ localVar.py
@@ -0,0 +1,8 @@
+def my_adder(a, b, c):
+    out = a + b + c
+    print(f"The value out within the function is {out}")
+    return out
+
+out = 1
+d = my_adder(1, 2, 3)
+print(f"The value out out of the function is {out}")
