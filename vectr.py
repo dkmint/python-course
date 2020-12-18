47cc7a2c50385b5bd0e0b14eb85535dfe4267eff
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2020-12-18 22:49:37 +0100

    Create vectr.py
    Vector demonstration.

---
+++ vectr.py
@@ -0,0 +1,5 @@
+import numpy as np
+vector_row = np.array([[1, -5, 3, 2, 4]])         # 1 row and 5 columns
+vector_column = np.array([[1], [2], [3], [4]])    # 4 rows and 1 column
+print(vector_row.shape)
+print(vector_column.shape)
