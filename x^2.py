723b138f38495bac3fc49c1b0277964803d37bf3
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2021-01-16 22:49:50 +0100

    Plots y = x^2 function

---
+++ x^2.py
@@ -0,0 +1,9 @@
+import numpy as np, matplotlib.pyplot as plt
+num_points = 5
+x_min, x_max = 0, 4
+x_values = np.linspace(x_min, x_max, num_points)
+y_values = x_values**2
+plt.plot(x_values, y_values)
+plt.show()
+
+# print(np.sqrt(2))
