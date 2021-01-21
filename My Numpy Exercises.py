45eb44098018661a1e99197a1596120600c22d2c
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2021-01-21 09:47:11 +0100

    NumPy Exercises # 1

---
+++ My Numpy Exercises.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+arr = np.zeros(10)
+arr
+
+
+# In[7]:
+
+
+ar = np.ones(10)
+ar
+
+
+# In[12]:
+
+
+arr5 = np.zeros(10)
+arr5[:]=5
+arr5
+
+
+# In[13]:
+
+
+arr10 = np.arange(10, 51)
+arr10
+
+
+# In[14]:
+
+
+arrEven = np.arange(10, 51, 2)
+arrEven
+
+
+# In[17]:
+
+
+arr_1d = np.arange(0,9)
+arr_2d = arr_1d.reshape(3,3)
+arr_2d
+
+
+# In[18]:
+
+
+arreye = np.eye(3,3)
+arreye
+
+
+# In[22]:
+
+
+np.random.rand()
+
+
+# In[23]:
+
+
+np.random.randn(26)
+
+
+# In[24]:
+
+
+np.linspace(0,101,100)
+
+
+# In[ ]:
+
+
+
+
